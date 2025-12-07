0224c67a99159e2f503e18ba13e90831d9c2ac1b
MuhammadFaleh <74877253+MuhammadFaleh@users.noreply.github.com>
2025-12-07 12:54:21 +0300

    Delete one_oneTeacher directory

--- one_oneTeacher/Api/ApiException.java
+++
@@ -1,8 +0,0 @@
-package com.JPARelation.one_oneTeacher.Api;
-
-public class ApiException extends RuntimeException{
-
-    public ApiException(String message){
-        super(message);
-    }
-}
--- one_oneTeacher/Repository/TeacherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.JPARelation.one_oneTeacher.Repository;
-
-import com.JPARelation.one_oneTeacher.Model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
-    Teacher findTeacherById(Integer id);
-}
